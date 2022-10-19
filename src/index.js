272bd5db114586fa0dc3290b8741b013fb63d435
Daira Felix <dairafelix@icloud.com>
2022-10-18 23:59:50 -0500

    Included name in text response

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There!' })
+  res.send({ msg: 'Knock Knock, whos where? Itsa ME! Daira :3' })
 })
 
 app.listen(port)

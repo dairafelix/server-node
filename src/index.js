71ff73e312ad7ffb09ee226bea7dcb8a7ab95655
Daira Felix <dairafelix@icloud.com>
2022-11-01 23:19:28 -0500

    Added Basic Auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
-import bodyParser from 'body-parser' 
+import bodyParser from 'body-parser'
+import basicAuth from 'express-basic-auth'
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -11,6 +12,11 @@ import { notFound, errorHandler } from './utils/errors'
 const port = parseInt(process.env.PORT, 10) || 3000
 const app = express()
 
+app.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+)
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())

48384ea2c9523ff9b0b0032e6793ec0ba5b2a2cc
Daira Felix <dairafelix@icloud.com>
2022-11-01 22:16:01 -0500

    Added Basic Model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
-
+import bodyParser from 'body-parser' 
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -14,6 +14,8 @@ const app = express()
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
+app.use(bodyParser.json())
+
 app.use(router)
 
 app.use(notFound)

65615a29e0dd8517e2826e3378426f064790024a
Daira Felix <dairafelix@icloud.com>
2022-11-13 19:18:11 -0600

    Added Department Creation

--- src/routes/api/companies.js
+++ src/routes/api/companies.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { getCompanies, getCompany, addCompany } from '../../models/companies'
+import { addDepartment } from '../../models/departments'
 
 const router = Router()
 
@@ -28,4 +29,19 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.post('/:id', async (req, res) => {
+  const companyId = req.params.id
+  const company = await getCompany(companyId)
+  if (company) {
+    const departmentName = req.body.name
+    if (departmentName) {
+      const department = await addDepartment(departmentName, companyId)
+      res.send(department)
+    } else {
+      res.status(400).send({ msg: 'Department name is required' })
+    }
+  } else {
+    res.status(400).send({ msg: 'Company does not exist required' })
+  }
+})
 export default router

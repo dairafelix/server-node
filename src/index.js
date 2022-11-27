c3c4a429179c35873114664ab566eba2db4c3ccf
Daira Felix <dairafelix@icloud.com>
2022-11-26 18:57:31 -0600

    Protected API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
+
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -12,13 +12,13 @@ import { notFound, errorHandler } from './utils/errors'
 const port = parseInt(process.env.PORT, 10) || 3000
 const app = express()
 
+app.use(morgan(process.env.MORGAN_LOG))
 app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  cors({
+    origin: process.env.ORIGIN,
+    exposedHeaders: ['x-total-count', 'x-total-pages'],
   }),
 )
-app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
 app.use(bodyParser.json())
 
